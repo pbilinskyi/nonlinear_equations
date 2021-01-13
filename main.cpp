fda1f618215a71f332e21b9238053ccf3043516d
ibilinskij_at_gmail.com <git@basecamp.globallogic.com.ua>
2021-01-13 15:20:06 +0200

    minor improvements

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ double iterative_process_fixed(double x_0, double n_apr){
     int n = 0;
     bool flag = true;
 
-    std::cout << "n       x_n       |x_n - x_n-1|      f(x_n)" << std::endl;
+    std::cout << " n         x_n         |x_n - x_n-1|        f(x_n)" << std::endl;
     while ((n < n_apr) || ((err > eps) || (std::abs(f(x_prev)) > eps))){
         ++n;
         x_next = phi(x_prev);
         err = std::abs(x_next - x_prev);
         x_prev = x_next;
         // printing table
-        printf("%d    %f       %f        %f", n, x_next, err, f(x_next));
+        printf("%2d %15.6f %15.6f %15.6f", n, x_next, err, f(x_next));
         if ((err < eps) && (std::abs(f(x_next)) < eps) && flag){
             std::cout << " <--" << std::endl;
             flag = false;
@@ -90,15 +90,19 @@ int main() {
     std::cout << "==Fixed point iteration==" << std::endl;
     int n_apr = 27; // CHANGE THIS NUMBER!
     double x_fixed = iterative_process_fixed(a, n_apr);
-    std::cout << "x = " << x_fixed << std::endl;
+    printf("%.10f\n", x_fixed);
+//    std::cout << "x = " << x_fixed << std::endl;
 
     std::cout << "==Newton modified========" << std::endl;
     double x_newton_mod  = iterative_process_Newton_mod(a, 100);
-    std::cout << "x = " << x_newton_mod << std::endl;
+    printf("%.10f\n", x_newton_mod);
+//    std::cout << "x = " << x_newton_mod << std::endl;
 
     std::cout << "==Hordes=================" << std::endl;
     double x_hord  = iterative_process_hordes(a, (a + b)/2, 100);
-    std::cout << "x = " << x_fixed << std::endl;
+    printf("%.10f\n", x_hord);
+
+//    std::cout << "x = " << x_fixed << std::endl;
 
     return 0;
 }
