cd4c3f67bf76348d0eb72d84ef14d5aacf625546
ibilinskij_at_gmail.com <git@basecamp.globallogic.com.ua>
2021-01-13 14:24:50 +0200

    minor improvements

--- main.cpp
+++ main.cpp
@@ -26,12 +26,13 @@ double iterative_process(double x_0, double n_max){
     double err = std::abs(x_next - x_prev);
     int n = 0;
 
-    std::cout << "n    x_n          x_n-1         f(x_n)" << std::endl;
+    std::cout << "n       x_n       |x_n - x_n-1|      f(x_n)" << std::endl;
     while ((n < n_max) && ((err > eps) || (std::abs(f(x_prev)) > eps))){
-        x_next = phi(x_prev);
-        printf("%d    %f     %f     %f\n", n, x_next, x_prev, f(x_next));
-        x_prev = x_next;
         ++n;
+        x_next = phi(x_prev);
+        err = std::abs(x_next - x_prev);
+        printf("%d    %f       %f        %f\n", n, x_next, err, f(x_next));
+        x_prev = x_next;
     }
     return x_next;
 }
